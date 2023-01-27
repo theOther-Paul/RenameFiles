3332ac78c66332394214ec9204c8724806153550
Paul <paulflorentin.pf@gmail.com>
2023-01-27 08:51:59 +0200

    small modifications and formating

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # RenameFiles
 
-Will rename all excel files in a folder based on extensions and a specified cell contents, in this case, a date. 
+## General use and description
 
-This is a program used to simpify the workflow in a work related situation where we needed to rename a lot of files, based on the above rule, in order for the files to be correctly grabbed by a VBA macro i've implemented. 
+Will rename all excel files in a folder based on extensions and a specified cell contents, in this case, a date.
 
-This process shaved 20-30 minutes from an agent's time and drastically improved the accuracy and limited the possible mistakes like double download, wrong name format, wrong save path, not downloading the file at all and eliminated the need to open each file individually. 
+This is a program used to simplify the workflow in a work related situation where we needed to rename a lot of files, based on the above rule, in order for the files to be correctly grabbed by a VBA macro i've implemented.
+
+This process shaved 20-30 minutes from an agent's time and drastically improved the accuracy and limited the possible mistakes like double download, wrong name format, wrong save path, not downloading the file at all and eliminated the need to open each file individually.
+
+## Installation process
+
+This script can be transformed in a ```.exe``` file and used as a standalone application, via this [pyinstaller manual](https://pyinstaller.org/en/stable/)
+
+## Dependencies
+
+Requirements and dependencies will be installed via ```requirements.txt``` file: <br>
+> ```pip install -r requirements.txt```
+
+## Status
+
+This application is now deprecated due to process changes, but it can be repurposed for general use in the same manner, following the same rule as above
--- smallComponentsTest.py
+++ smallComponentsTest.py
@@ -5,23 +5,22 @@ import pathlib
 import re
 from os import devnull
 
-# global variables
-
 
 def format_date(extracted_string):
-    new_s = ''
+    new_s = ""
     wip_s = re.sub(r"[^a-zA-Z0-9 ]", "", extracted_string)
-    wip_s = ''.join(c for c in wip_s if c.isnumeric())
-    wip_s = wip_s[:len(wip_s)//2]
+    wip_s = "".join(c for c in wip_s if c.isnumeric())
+    wip_s = wip_s[: len(wip_s) // 2]
+
     # will slice at 4th index and the 6th and will concatenate the remaining 3 before 4th and the rest after 5th
-    extracted_string = wip_s[:4]+wip_s[6:]
+    extracted_string = wip_s[:4] + wip_s[6:]
     return extracted_string
 
 
 def extract_date(path):  # needs to get the full path of the file from the folder
     try:
-        df = pd.read_excel(path, sheet_name='Synthèse')  # df=dataframe
-        return format_date(df['Unnamed: 1'].values[3])
+        df = pd.read_excel(path, sheet_name="Synthèse")  # df=dataframe
+        return format_date(df["Unnamed: 1"].values[3])
 
     except OSError as e:
         print(e)
@@ -30,19 +29,21 @@ def extract_date(path):  # needs to get the full path of the file from the folde
 def main():
     SPEC_PATH = input("path: ")
     SPEC_PATH = pathlib.Path(SPEC_PATH).resolve()  # will access the given path
-    #SPEC_PATH = SPEC_PATH.replace("\\", r'/')
     file_count = sum(len(files) for _, _, files in os.walk(SPEC_PATH))
     counter = 0
     user_choice = input(
-        "Would you like the new name to contain a string before the date?(y/n) ").lower()
+        "Would you like the new name to contain a string before the date?(y/n) "
+    ).lower()
 
     while counter <= file_count:
         if user_choice.isnumeric():
-            print("Please enter a valid option!\nAvailable options are 'y' or 'n'.\nThe program will rerun to validate your data.")
+            print(
+                "Please enter a valid option!\nAvailable options are 'y' or 'n'.\nThe program will rerun to validate your data."
+            )
             return main()
         else:
             if user_choice.isalpha():
-                if user_choice == 'y':
+                if user_choice == "y":
                     head_string = input("Please enter the string: ")
 
                     # renaming operation
@@ -52,24 +53,28 @@ def main():
                         custom_name = extract_date(full_path)
                         new_name = f"{head_string}{custom_name}{extension}"
                         try:
-                            os.rename(os.path.join(SPEC_PATH, x),
-                                      os.path.join(SPEC_PATH, new_name))
+                            os.rename(
+                                os.path.join(SPEC_PATH, x),
+                                os.path.join(SPEC_PATH, new_name),
+                            )
                             print("Success!")
                             counter += 1
                         except OSError as e:
                             print(e)
                             counter += 1
                 else:
-                    if user_choice == 'n':
+                    if user_choice == "n":
                         for x in os.listdir(SPEC_PATH):
                             full_path = os.path.join(SPEC_PATH, x)
                             extension = pathlib.Path(full_path).suffix
                             custom_name = extract_date(full_path)
-                            head_string = ''
+                            head_string = ""
                             new_name = f"{head_string}{custom_name}{extension}"
                             try:
-                                os.rename(os.path.join(SPEC_PATH, x),
-                                          os.path.join(SPEC_PATH, new_name))
+                                os.rename(
+                                    os.path.join(SPEC_PATH, x),
+                                    os.path.join(SPEC_PATH, new_name),
+                                )
                                 print("Success!")
                                 counter += 1
                             except OSError as e:
@@ -80,5 +85,5 @@ def main():
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
